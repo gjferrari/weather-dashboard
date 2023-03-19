4ca7659526f0feffa4429a23ccc203a21ec23f7c
Genevieve Ferrari <genferrari@gmail.com>
2023-03-18 23:04:49 -0700

    skeleton script for todayWeather function & correlated changes on html

--- develop/script.js
+++ develop/script.js
@@ -1,29 +1,48 @@
-console.log("testing");
-var searchWeather = document.getElementById("searchWeather");
-var clearHistory = document.getElementById("clearHistory");
-var APIKey = "a2177dabb6b2705fae8ab52208e93f5c";
-var city;
-var nextFiveDays = document.getElementById("nextFiveDays");
-var todayWeather = document.getElementById("todayWeather");
-var infoCard = document.querySelector("#infocard");
+//switching to jQuery
+const APIKey = "a2177dabb6b2705fae8ab52208e93f5c";
 
-//array for previously stored cities
-var oldcities = [];
+const searchHistory = [];
 
-//API
-//api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={APIKey}
+const today = moment().format();
 
-const temp = 0;
-const city = "";
-const humidity = "";
-const wind = "";
-
-searchWeather.addEventListener("click", function (event) {
-  console.log("search button clicked");
-  event.preventDefault();
-  handleUserInput();
-});
-
-const start = () => {
-  const savedSearch = JSON.parse(localStorage.getItem("search-history"));
+const todayWeather = (city) => {
+  const todayWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+  $.ajax({
+    url: todayWeatherURL,
+    method: "GET",
+  }).then((todayResponse) => {
+    $("#infoCard").css("display", "block");
+    $("cityName").empty();
+    var iconCode = currentResponse.weather[0].icon;
+    var weatherIconURL = `https://openweathermap.org/img/w/${iconCode}.png`;
+  });
 };
+
+// var searchWeather = document.getElementById("searchWeather");
+// var clearHistory = document.getElementById("clearHistory");
+
+// var city;
+// var nextFiveDays = document.getElementById("nextFiveDays");
+// var todayWeather = document.getElementById("todayWeather");
+// var infoCard = document.querySelector("#infocard");
+
+// //array for previously stored cities
+// var oldcities = [];
+
+// //API
+// //api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={APIKey}
+
+// const temp = 0;
+// const city = "";
+// const humidity = "";
+// const wind = "";
+
+// searchWeather.addEventListener("click", function (event) {
+//   console.log("search button clicked");
+//   event.preventDefault();
+//   handleUserInput();
+// });
+
+// const start = () => {
+//   const savedSearch = JSON.parse(localStorage.getItem("search-history"));
+// };
